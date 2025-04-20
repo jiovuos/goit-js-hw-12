60a6850c903356cdea685c598b6aa5870bdd5a54
Volodymyr <128148818+jiovuos@users.noreply.github.com>
2025-04-20 21:06:26 +0300

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '39486318-698ac6813de625d5bba6b85f3';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
